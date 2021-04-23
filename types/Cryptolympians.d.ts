db73b13bd28ce5294ea4a7cefa6db96c576b8690
eddie dugan <ejd4@protonmail.com>
2021-04-23 10:49:47 -0700

    update ropsten contract address, filter bid events by auction index

--- types/Cryptolympians.d.ts
+++ types/Cryptolympians.d.ts
@@ -215,7 +215,7 @@ interface CryptolympiansInterface extends ethers.utils.Interface {
   events: {
     "Approval(address,address,uint256)": EventFragment;
     "ApprovalForAll(address,address,bool)": EventFragment;
-    "Bid(address,uint256)": EventFragment;
+    "Bid(address,uint256,uint256)": EventFragment;
     "OwnershipTransferred(address,address)": EventFragment;
     "Transfer(address,address,uint256)": EventFragment;
   };
@@ -1085,11 +1085,12 @@ export class Cryptolympians extends Contract {
     >;
 
     Bid(
-      from: null,
-      amount: null
+      from: string | null,
+      amount: null,
+      auctionIndex: BigNumberish | null
     ): TypedEventFilter<
-      [string, BigNumber],
-      { from: string; amount: BigNumber }
+      [string, BigNumber, BigNumber],
+      { from: string; amount: BigNumber; auctionIndex: BigNumber }
     >;
 
     OwnershipTransferred(
